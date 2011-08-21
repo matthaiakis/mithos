f5370f801db080b452dd29f6c40a587564c4fdda
mithos <mithos@atmos.(none)>
2011-08-21 18:44:48 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mithos::Application.config.secret_token = '25387df62ad9146684a36fe142068b8e3529d5c830c87370f1b1a17816b3f054a1201286887a6af50025a7662e3a610e5b0c0b41691dd93d3f8f9e78ac76ecdb'
